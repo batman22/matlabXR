97979eb7fcd6d2fa42362b8bec5fa8fe79d59cea
Alino <73147643+alino93@users.noreply.github.com>
2024-06-21 16:54:29 -0400

    Samples of creating 3D objects in MATLAB

--- samples/sample_bar3.m
+++ samples/sample_bar3.m
@@ -1,7 +1,3 @@
-% © Copyright 2014-2023 Rohan Chabukswar
-%
-% This file is part of MATLAB GLTF.
-%
 % MATLAB GLTF is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or (at
